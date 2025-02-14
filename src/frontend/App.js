894d057d4aa6553ba0a20fcf0c719d6c5aadbf23
Gustavo <pow10@txstate.edu>
2025-02-13 20:03:11 -0600

    Add initial React app structure with Navbar component and homepage

---
+++ src/frontend/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Home</h1>
+        </>
+    );
+};
+
+export default App;
---
+++ src/frontend/components/Navbar.jsx
@@ -0,0 +1,17 @@
+// components/Navbar.js
+import React from "react";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">MyApp</div>
+            <div className="nav-links">
+                <button className="btn login">Login</button>
+                <button className="btn get-started">Get Started</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
