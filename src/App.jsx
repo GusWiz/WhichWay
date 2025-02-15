3d44f949805b86a487935a0717f50e871e8ecac5
Alan De La Torre <sua9@txstate.edu>
2025-02-15 15:22:26 -0600

    WW-5 learning react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Header from './components/Header.jsx';
-
+import Login from './components/Login.jsx';
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
 
 export default App;
