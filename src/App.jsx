9409d9b7ffe2804fb96d7aeab5ded40f3338573e
vrsecode <158780886+vrsecode@users.noreply.github.com>
2025-02-17 23:18:31 -0600

    WW-38 more work on menu bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import Signup from './components/Login-Components/Signup.jsx';
 import ForgotPassword from './components/Login-Components/ForgotPassword.jsx';
 import EnterVerificationCode from './components/Login-Components/EnterVerificationCode.jsx';
 import ResetPassword from './components/Login-Components/ResetPassword.jsx';
-import Navbar from './components/Page-Components/Navbar.jsx';
+import Navbar from './Components/Page-Components/Navbar.jsx';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Router>
       <Navbar />
+      <Switch>
+        <Route path='/' />
+      </Switch>
+    </Router>
     </>
   );
 }
--- src/Components/Page-Components/Navbar.jsx
+++ src/Components/Page-Components/Navbar.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as GrIcons from 'react-icons/gr';
 import { Link } from 'react-router-dom';
+import { SidebarData } from "./SidebarData.jsx";
+import "../App.css";
+import { IconContext } from "react-icons";
 
 function Navbar() {
-    return (
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(sidebar);
+
+    return(
         <>
-            <div className='navbar'>
-                <Link to='#' className='menu-bars'>
-                    <FaIcons.FaBars />
-                </Link>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-items'>
-                    <li className="navbar-toggle">
-                        <Link to="#" className='menu-bars'>
-                            <GrIcons.GrClose />
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
+            <IconContext.Provider value={{color: "undefined"}}>
+                <div className="navbar">
+                    <Link to ="#" className="menu-bars">
+                        <FaIcons.FaBars onClickCapture={showSidebar} />
+                    </Link>
+                </div>
+                <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+                    <ul className="nav-menu-items" onClicl={showSidebar}>
+                        <li className="navbar-toggle">
+                            <Link to="#" className="menu-bars">
+                                <GrIcons.GrClose />
+                            </Link>
+                        </li>
+                        {SidebarData.map((item, index) => {
+                            return (
+                                <li key={index} className={item.className}>
+                                    <Link to={item.path}>
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+            </IconContext.Provider>
         </>
-    );
+    )
 
 }
 
