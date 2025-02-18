66998f61410e365aa9929dab203cfc4033d0db4b
vrsecode <158780886+vrsecode@users.noreply.github.com>
2025-02-17 22:42:27 -0600

    WW-38 Work on menu bar

--- src/Components/Page-Components/Navbar.jsx
+++ src/Components/Page-Components/Navbar.jsx
@@ -1,9 +1,28 @@
-
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as GrIcons from 'react-icons/gr';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <>
+            <div className='navbar'>
+                <Link to='#' className='menu-bars'>
+                    <FaIcons.FaBars />
+                </Link>
+            </div>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items'>
+                    <li className="navbar-toggle">
+                        <Link to="#" className='menu-bars'>
+                            <GrIcons.GrClose />
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </>
     );
 
 }
+
+export default Navbar;
