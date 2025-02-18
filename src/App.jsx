48e2ca95c0888d14c8ac6c7d739cd5880784c73b
vrsecode <158780886+vrsecode@users.noreply.github.com>
2025-02-17 22:14:18 -0600

    WW-38 Created base file for Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Signup from './components/Login-Components/Signup.jsx';
 import ForgotPassword from './components/Login-Components/ForgotPassword.jsx';
 import EnterVerificationCode from './components/Login-Components/EnterVerificationCode.jsx';
 import ResetPassword from './components/Login-Components/ResetPassword.jsx';
+import Navbar from './components/Page-Components/Navbar.jsx';
 
 function App() {
   return (
     <>
-      <ResetPassword />
+      <Navbar />
     </>
   );
 }
