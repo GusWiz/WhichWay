a3c61078dbc8f3c0952e97d970d13adb9733c4f9
Gustavo <pow10@txstate.edu>
2025-02-18 20:48:24 -0600

    WW-37 Added navigation bar component with login and get started buttons

---
+++ src/components/navigationBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function navigationBar() {
+  return (
+    <div>
+      <nav classname="navbar">
+            <ul>
+                <fa-ul><button onclick="location.href''">Login</button></fa-ul>
+                <fa-ul><button onclick="location.href''">Get Started</button></fa-ul>            
+            </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default navigationBar;
