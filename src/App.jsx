20da126c18b2cab0bb97360be4fd74f8d9fca6ae
vrsecode <158780886+vrsecode@users.noreply.github.com>
2025-02-18 22:56:38 -0600

    WW-38 trying to merge

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-    </Router>
+    <ResetPassword />
     </>
   );
 }
