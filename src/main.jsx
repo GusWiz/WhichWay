9930aaa5083c9adce86aa1582c562e950f337b49
Aldo Guerrero <nrd53@txstate.edu>
2025-02-19 14:41:57 -0600

    WW-20 Updated TriponHome.css and TriponHome.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './login-styling.css';
+//import './login-styling.css';
 import './TriponHome.css';
 import App from './App.jsx';
 
