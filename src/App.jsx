40fc17de15cffd58f897814d03e572511c42fa23
Aldo Guerrero <nrd53@txstate.edu>
2025-02-19 11:13:43 -0600

    WW-20 Deugging TriponHome.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Signup from './components/Login-Components/Signup.jsx';
 import ForgotPassword from './components/Login-Components/ForgotPassword.jsx';
 import EnterVerificationCode from './components/Login-Components/EnterVerificationCode.jsx';
 import ResetPassword from './components/Login-Components/ResetPassword.jsx';
-
+import TriponHome from './Components/TripHomePage/TriponHome.jsx';
 function App() {
   return (
     <>
-      <Login />
+      <TriponHome />
     </>
   );
 }
