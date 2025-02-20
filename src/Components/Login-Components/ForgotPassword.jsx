c706389569697f2d47e783b9f6e390c6b177b07d
Alan De La Torre <sua9@txstate.edu>
2025-02-20 12:03:42 -0600

    WW-5 finished reset password functionality

--- src/Components/Login-Components/ForgotPassword.jsx
+++ src/Components/Login-Components/ForgotPassword.jsx
@@ -25,18 +25,14 @@ function ForgotPassword() {
       <div className='container'>
         <h2 className='form-title'>Enter Your Email</h2>
 
-        <form className='form'>
+        <form className='form' onSubmit={handleReset}>
           <InputField
             type='email'
             placeholder='Email Address'
             onChange={(e) => setEmail(e.target.value)}
           />
 
-          <LoginButton
-            text='Send Verification Code'
-            onClick={handleReset}
-            type='button'
-          />
+          <LoginButton text='Send Email' />
         </form>
       </div>
     </>
