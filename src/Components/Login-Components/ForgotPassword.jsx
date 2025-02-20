a6cb81553e82e07ea9e49a15ab37424ea6caf3f5
Alan De La Torre <sua9@txstate.edu>
2025-02-19 22:17:43 -0600

    WW-5 working on forgot password func

--- src/Components/Login-Components/ForgotPassword.jsx
+++ src/Components/Login-Components/ForgotPassword.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import LoginButton from './LoginButton';
 import InputField from '../InputField';
 
 function ForgotPassword() {
+  const [email, setEmail] = useState('');
+
+  const handleReset = async (e) => {
+    e.preventDefault();
+
+    const auth = getAuth();
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert('email sent');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  };
   return (
     <>
       <div className='container'>
         <h2 className='form-title'>Enter Your Email</h2>
 
-        <form action='#' className='form'>
-          <InputField type='email' placeholder='Email Address' />
+        <form className='form'>
+          <InputField
+            type='email'
+            placeholder='Email Address'
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-          <LoginButton text='Send Verification Code' />
+          <LoginButton
+            text='Send Verification Code'
+            onClick={handleReset}
+            type='button'
+          />
         </form>
       </div>
     </>
