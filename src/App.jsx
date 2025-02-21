46e1a5725b53f6c0851948b328114e61f016eb0c
Alan De La Torre <sua9@txstate.edu>
2025-02-21 15:57:18 +0000

    App.jsx edited online with Bitbucket

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { auth } from './Components/firebase.js';
+import { auth } from './components/firebase.js';
 import {
   BrowserRouter as Router,
   Routes,
