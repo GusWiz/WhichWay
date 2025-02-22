d74f09179633d4b726f3bc1b6189099094172eab
Alan De La Torre <sua9@txstate.edu>
2025-02-21 21:15:55 -0600

    WW-40 workable and intuitive sidebar

--- src/components/RoutingPage/HomeBody.jsx
+++ src/components/RoutingPage/HomeBody.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../../HomeBody.css';
+import Sidebar from '../Homepage-Components/Sidebar';
 
 // Mising proper routing after the buttons are pressed
 function HomeBody() {
   return (
-    <main className="home-body"> 
-      <div className="option-boxes"> 
-        <button onClick={() => history.push('/create-trip')}>Create New Trip</button>
-        <button onClick={() => history.push('/load-trip')}>Load Existing Trip</button>
-      </div>
-    </main>
+    <>
+      <Sidebar />
+      <main className='home-body'>
+        <div className='option-boxes'>
+          <button onClick={() => history.push('/create-trip')}>
+            Create New Trip
+          </button>
+          <button onClick={() => history.push('/load-trip')}>
+            Load Existing Trip
+          </button>
+        </div>
+      </main>
+    </>
   );
 }
 
