f0e5aabaa5211ebca58b7713ca0ba0ecd6069ab6
asiemsen <siemsenaaron@gmail.com>
2025-02-23 22:11:13 -0600

    fixed merge conflicts

--- src/components/RoutingPage/HomeBody.jsx
+++ src/components/RoutingPage/HomeBody.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import '../../HomeBody.css';
 import NavigationBar from './NavigationBar';
+import { useNavigate } from 'react-router-dom';
 
 // Mising proper routing after the buttons are pressed
 function HomeBody() {
+  const navigate = useNavigate();
+
   return (
     <>
       <NavigationBar />
       <main className='home-body'>
         <div className='option-boxes'>
-          <button onClick={() => history.push('/create-trip')}>
+          <button onClick={() => navigate('/create-trip')}>
             Create New Trip
           </button>
-          <button onClick={() => history.push('/load-trip')}>
+          <button onClick={() => navigate('/load-trip')}>
             Load Existing Trip
           </button>
         </div>
