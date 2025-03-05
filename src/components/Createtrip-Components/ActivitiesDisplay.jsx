c59dc90369cdb78aad48d102a9c24aabe9a0c9fc
asiemsen <siemsenaaron@gmail.com>
2025-03-05 12:41:18 -0600

    WW-31 Created new component for activities display and inserted into CreateTrip.jsx

---
+++ src/components/Createtrip-Components/ActivitiesDisplay.jsx
@@ -0,0 +1,125 @@
+// ActivitiesDisplay.jsx
+import React from 'react';
+import PropTypes from 'prop-types'; // For type checking (optional)
+import './ActivitiesDisplay.css';
+
+function ActivitiesDisplay({
+  foodOptions,
+  selectedFoods,
+  handleSelectFood,
+  entertainmentOptions,
+  selectedEntertainment,
+  handleSelectEntertainment,
+  outdoorOptions,
+  selectedOutdoor,
+  handleSelectOutdoor
+}) {
+  return (
+    <div className='activities-container'>
+      <h2>Activities</h2>
+
+      <div className='categories-container'>
+        {/* Entertainment Selection */}
+        <div className='category'>
+          <h2 className='form-title'>Entertainment</h2>
+          <div className='selectable-container'>
+            {entertainmentOptions.map((item) => (
+              <label
+                key={item.name}
+                className={`selectable-box ${
+                  selectedEntertainment.includes(item.name) ? 'selected' : ''
+                }`}
+              >
+                <input
+                  type='checkbox'
+                  name='entertainment'
+                  value={item.name}
+                  checked={selectedEntertainment.includes(item.name)}
+                  onChange={() => handleSelectEntertainment(item.name, item.price)}
+                />
+                <img
+                  src={item.imgSrc}
+                  alt={item.name}
+                  className='selectable-image'
+                />
+                <span className='selectable-price'>${item.price}</span>
+              </label>
+            ))}
+          </div>
+        </div>
+
+        {/* Food Selection */}
+        <div className='category'>
+          <h2 className='form-title'>Food</h2>
+          <div className='selectable-container'>
+            {foodOptions.map((food) => (
+              <label
+                key={food.name}
+                className={`selectable-box ${
+                  selectedFoods.includes(food.name) ? 'selected' : ''
+                }`}
+              >
+                <input
+                  type='checkbox'
+                  name='food'
+                  value={food.name}
+                  checked={selectedFoods.includes(food.name)}
+                  onChange={() => handleSelectFood(food.name, food.price)}
+                />
+                <img
+                  src={food.imgSrc}
+                  alt={food.name}
+                  className='selectable-image'
+                />
+                <span className='selectable-price'>${food.price}</span>
+              </label>
+            ))}
+          </div>
+        </div>
+
+        {/* Outdoor Selection */}
+        <div className='category'>
+          <h2 className='form-title'>Outdoor</h2>
+          <div className='selectable-container'>
+            {outdoorOptions.map((item) => (
+              <label
+                key={item.name}
+                className={`selectable-box ${
+                  selectedOutdoor.includes(item.name) ? 'selected' : ''
+                }`}
+              >
+                <input
+                  type='checkbox'
+                  name='outdoor'
+                  value={item.name}
+                  checked={selectedOutdoor.includes(item.name)}
+                  onChange={() => handleSelectOutdoor(item.name, item.price)}
+                />
+                <img
+                  src={item.imgSrc}
+                  alt={item.name}
+                  className='selectable-image'
+                />
+                <span className='selectable-price'>${item.price}</span>
+              </label>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ActivitiesDisplay.propTypes = {
+  foodOptions: PropTypes.array.isRequired,
+  selectedFoods: PropTypes.array.isRequired,
+  handleSelectFood: PropTypes.func.isRequired,
+  entertainmentOptions: PropTypes.array.isRequired,
+  selectedEntertainment: PropTypes.array.isRequired,
+  handleSelectEntertainment: PropTypes.func.isRequired,
+  outdoorOptions: PropTypes.array.isRequired,
+  selectedOutdoor: PropTypes.array.isRequired,
+  handleSelectOutdoor: PropTypes.func.isRequired
+};
+
+export default ActivitiesDisplay;
