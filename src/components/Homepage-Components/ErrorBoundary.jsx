1b26abee6ab297296f5c6a5219ab460f8190dd81
Gustavo <pow10@txstate.edu>
2025-03-05 23:11:20 -0600

    Merge branch 'WW-93-development-create-development-branch-with-standardized-file-structure' of https://bitbucket.org/cs3398-betazoids-s25/whichway into WW-24-task-5-unit-testing-for-backend-fr

---
+++ src/components/Homepage-Components/ErrorBoundary.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+// class handles errors for component
+class ErrorBoundary extends Component {
+    // default constructor that sets the error state to false
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+    // static method that returns true if error is present
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+    // method that logs error to console
+    componentDidCatch(error, errorInfo) { // log error to console
+        console.error('ErrorBoundary caught an error', error, errorInfo); // log error to console
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Something went wrong.</h1>; // return error message
+        }
+
+        return this.props.children; // return children
+    }
+}
+
+export default ErrorBoundary;
