503963646f207e57bd8c0d4b8ffeee5a10373677
asiemsen <siemsenaaron@gmail.com>
2025-03-18 11:24:30 -0500

    WW-72 formatted with make fmt

--- src/pages/CreateTrip.jsx
+++ src/pages/CreateTrip.jsx
@@ -60,17 +60,15 @@ function CreateTrip() {
   const budgetSubmit = (event) => {
     event.preventDefault();
     if (details.budget < 0) {
-      return toast("Error: Invalid Budget Entered.");
-    } 
-    else if (details.budget < displayedCost.cost){
-      return toast("Error: Budget would be less than Cost.");
-    }
-    else{
+      return toast('Error: Invalid Budget Entered.');
+    } else if (details.budget < displayedCost.cost) {
+      return toast('Error: Budget would be less than Cost.');
+    } else {
       setDisplayedBudget((prev) => {
         return { ...prev, budget: details.budget };
       });
     }
-    
+
     console.log(details);
   };
   // end of Vinny's functions
@@ -82,55 +80,61 @@ function CreateTrip() {
   const [selectedOutdoor, setSelectedOutdoor] = useState([]);
 
   const handleSelect = (category, value, price) => {
-      switch (category) {
-        case 'food':
-          if (displayedBudget.budget - displayedCost.cost - price < 0 && !selectedFoods.includes(value)) {
-            return toast("Error: Cost would be more than Budget.");
-          }
-          else {
-            selectedFoods.includes(value)
-              ? handleCostChange(price * -1)
-              : handleCostChange(price);
-            setSelectedFoods((prev) =>
-              prev.includes(value)
-                ? prev.filter((item) => item !== value)
-                : [...prev, value]
-            );
-          }
-          break;
-        case 'entertainment':
-          if (displayedBudget.budget - displayedCost.cost - price < 0 && !selectedEntertainment.includes(value)) {
-            return toast("Error: Cost would be more than Budget.");
-          }
-          else {
-            selectedEntertainment.includes(value)
-              ? handleCostChange(price * -1)
-              : handleCostChange(price);
-            setSelectedEntertainment((prev) =>
-              prev.includes(value)
-                ? prev.filter((item) => item !== value)
-                : [...prev, value]
-            );
-          }
-          break;
-        case 'outdoor':
-          if (displayedBudget.budget - displayedCost.cost - price < 0 && !selectedOutdoor.includes(value)) {
-            return toast("Error: Cost would be more than Budget.");
-          }
-          else {
-            selectedOutdoor.includes(value)
-              ? handleCostChange(price * -1)
-              : handleCostChange(price);
-            setSelectedOutdoor((prev) =>
-              prev.includes(value)
-                ? prev.filter((item) => item !== value)
-                : [...prev, value]
-            );
-          }
-          break;
-        default:
-          break;
-      }
+    switch (category) {
+      case 'food':
+        if (
+          displayedBudget.budget - displayedCost.cost - price < 0 &&
+          !selectedFoods.includes(value)
+        ) {
+          return toast('Error: Cost would be more than Budget.');
+        } else {
+          selectedFoods.includes(value)
+            ? handleCostChange(price * -1)
+            : handleCostChange(price);
+          setSelectedFoods((prev) =>
+            prev.includes(value)
+              ? prev.filter((item) => item !== value)
+              : [...prev, value]
+          );
+        }
+        break;
+      case 'entertainment':
+        if (
+          displayedBudget.budget - displayedCost.cost - price < 0 &&
+          !selectedEntertainment.includes(value)
+        ) {
+          return toast('Error: Cost would be more than Budget.');
+        } else {
+          selectedEntertainment.includes(value)
+            ? handleCostChange(price * -1)
+            : handleCostChange(price);
+          setSelectedEntertainment((prev) =>
+            prev.includes(value)
+              ? prev.filter((item) => item !== value)
+              : [...prev, value]
+          );
+        }
+        break;
+      case 'outdoor':
+        if (
+          displayedBudget.budget - displayedCost.cost - price < 0 &&
+          !selectedOutdoor.includes(value)
+        ) {
+          return toast('Error: Cost would be more than Budget.');
+        } else {
+          selectedOutdoor.includes(value)
+            ? handleCostChange(price * -1)
+            : handleCostChange(price);
+          setSelectedOutdoor((prev) =>
+            prev.includes(value)
+              ? prev.filter((item) => item !== value)
+              : [...prev, value]
+          );
+        }
+        break;
+      default:
+        break;
+    }
   };
 
   const foodOptions = [
@@ -157,158 +161,17 @@ function CreateTrip() {
     { name: 'Vinny Rosy River', imgSrc: 'river.jpg', price: '10' },
     { name: 'Alan De Le Torre Lake', imgSrc: 'lake.jpg', price: '5' },
   ];
-
-  const [preferences, setPreferences] = useState({
-    maxPrice: '',
-    maxTravelTime: '',
-    groupSize: '',
-    foodType: ''
-  });
-  
-  const handlePreferenceChange = (event) => {
-    const { name, value } = event.target;
-    setPreferences(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    //console.log('User Preferences:', preferences);
-    // Handle form submission logic (save to database/send to api call)
-  };
   // End of Aaron's functions
 
   return (
     <>
-      <div className='title-container'>
-        <h1>Create Trip</h1>
-      </div>
-
-      <div>
-        <form action='#' className='form'>
-          <InputField type='text' placeholder='Trip Name' />
-          <InputField type='text' placeholder='Destination' />
-          <InputField type='text' placeholder='Duration (days)' />
-        </form>
-        <label>Budget = $</label>
-        <label id='displayedBudget'>{displayedBudget.budget}</label>
-        <br></br>
-        <label>Cost = $</label>
-        <label id='displayedCost'>{displayedCost.cost}</label>
-        <br></br>
-        <label>Remaining Budget = $</label>
-        <label id='displayedRemainingBudget'>
-          {displayedBudget.budget - displayedCost.cost}
-        </label>
-        <form action='#' className='form' onSubmit={budgetSubmit}>
-          <input
-            type='number'
-            name='budget'
-            placeholder='Budget'
-            id='budgetInput'
-            onChange={handleChange}
-          />
-          <button type='submit'>Change Budget</button>
-        </form>
-
-        <div className='activities-container'>
-          <h2>Activities</h2>
-        </div>
-
-             {/* Activity Preferences */}
-        <div className="preferences-container">
-    <form className="activity-preferences-form" onSubmit={handleSubmit}>
-      <h3>Activity Preferences</h3>
-
-      {/* Maximum Price Input */}
-      <div className="input-group">
-        <label htmlFor="maxPrice">Maximum Price:</label>
-        <input
-          type="number"
-          id="maxPrice"
-          name="maxPrice"
-          value={preferences.maxPrice}
-          onChange={handlePreferenceChange}
-          placeholder="Enter maximum price"
-        />
-      </div>
-
-      {/* Maximum Travel Time Input */}
-      <div className="input-group">
-        <label htmlFor="maxTravelTime">Maximum Travel Time (in hours):</label>
-        <input
-          type="number"
-          id="maxTravelTime"
-          name="maxTravelTime"
-          value={preferences.maxTravelTime}
-          onChange={handlePreferenceChange}
-          placeholder="Enter travel time in hours"
-        />
-      </div>
-
-      {/* Group Size Input */}
-      <div className="input-group">
-        <label htmlFor="groupSize">Group Size:</label>
-        <input
-          type="number"
-          id="groupSize"
-          name="groupSize"
-          value={preferences.groupSize}
-          onChange={handlePreferenceChange}
-          placeholder="Enter group size"
-        />
-      </div>
-
-      {/* Food Type Input */}
-      <div className="input-group">
-        <label htmlFor="foodType">Food Type:</label>
-        <input
-          type="text"
-          id="foodType"
-          name="foodType"
-          value={preferences.foodType}
-          onChange={handlePreferenceChange}
-          placeholder="Enter preferred food type"
-        />
-      </div>
-
-      <button type="submit">Save Preferences</button>
-    </form>
-  </div>
-        
-
-        <div className='categories-container'>
-          {/* Entertainment Selection */}
-          <div className='category'>
-            <h2 className='form-title'>Entertainment</h2>
-            <div className='selectable-container'>
-              {entertainmentOptions.map((item) => (
-                <label
-                  key={item.name}
-                  className={`selectable-box ${
-                    selectedEntertainment.includes(item.name) ? 'selected' : ''
-                  }`}
-                >
-                  <input
-                    type='checkbox'
-                    name='entertainment'
-                    value={item.name}
-                    checked={selectedEntertainment.includes(item.name)}
-                    onChange={() =>
-                      handleSelect('entertainment', item.name, item.price)
-                    }
-                  />
-                  <img
-                    src={item.imgSrc}
-                    alt={item.name}
-                    className='selectable-image'
-                  />
-                  {/* <span className='selectable-title'>{item.name}</span> */}
-                  <span className='selectable-price'>${item.price}</span>
-                </label>
-              ))}
+      <NavigationBar />
+      <div className='home-page'>
+        <div className='home-container'>
+          <Sidebar logout={logout} />
+          <div className='home-contents'>
+            <div className='title-container'>
+              <h1>Create Trip</h1>
             </div>
 
             <div>
@@ -317,7 +180,11 @@ function CreateTrip() {
                 <InputField type='text' placeholder='Destination' />
                 <InputField type='text' placeholder='Duration' />
               </form>
-              <label>{displayedBudget.budget >= 0 ? "Budget = $" : "No budget entered."}</label>
+              <label>
+                {displayedBudget.budget >= 0
+                  ? 'Budget = $'
+                  : 'No budget entered.'}
+              </label>
               <label id='displayedBudget'>{displayedBudget.budget}</label>
               <br></br>
               <label>Cost = $</label>
@@ -358,11 +225,6 @@ function CreateTrip() {
               />
             </div>
 
-            <button 
-              type = 'button' >
-              save activities
-            </button>
-
             {/* Add button to open modal */}
             <button
               type='button'
@@ -372,14 +234,12 @@ function CreateTrip() {
               Trip Preferences
             </button>
 
-
             {/* Conditionally render the modal */}
             {isModalOpen && <PreferenceModal onClose={handleModalToggle} />}
             <ToastContainer />
           </div>
         </div>
       </div>
-      
     </>
   );
 }
