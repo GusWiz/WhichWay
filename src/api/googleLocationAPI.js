f66cc63d951105c9f8b93f81575c7232f559a0b5
Gustavo <pow10@txstate.edu>
2025-03-20 11:21:14 -0500

    WW-74 Implement Google Location API integration with Axios for place suggestions

--- src/api/googleLocationAPI.js
+++ src/api/googleLocationAPI.js
@@ -1,1 +1,23 @@
 // Searching for needs to complete HTTP request (for api calls)
+// Will be testing axious to complete HTTP client request
+import axious from 'axious';
+
+const GOOGLE_PLACE_API_URL = '';
+const GOOGLE_PLACE_DETAILS_API_URL = '';
+
+// given location it should call the google location API with all
+// details from the api.
+export const getPlaceSuggestions = async(input) => {
+    try {
+        const response = await.axious.get(GOOGLE_PLACES_API_URL, {
+            params: {
+                input,
+                key: process.env.REACT_APP_GOOGLE_API_KEY,
+            },
+        })
+        return response.data.predictions;
+    } catch (error) {
+        console.error('Error fetching place suggestions: ', error);
+        return []; // emply list
+    }
+}
