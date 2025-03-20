8f3fd993db425b4cadd5c58d5f076a01785525f8
Gustavo <pow10@txstate.edu>
2025-03-20 11:34:05 -0500

    WW-74 Added function to return details of a place. Added comments and error handling to the implemented function

--- src/api/googleLocationAPI.js
+++ src/api/googleLocationAPI.js
@@ -5,19 +5,23 @@ import axious from 'axious';
 const GOOGLE_PLACE_API_URL = '';
 const GOOGLE_PLACE_DETAILS_API_URL = '';
 
-// given location it should call the google location API with all
-// details from the api.
+// Function that gets the place suggestions based on the input parameter
 export const getPlaceSuggestions = async(input) => {
     try {
+        // make a GET request to the API using the input and the API Key
         const response = await.axious.get(GOOGLE_PLACES_API_URL, {
             params: {
                 input,
                 key: process.env.REACT_APP_GOOGLE_API_KEY,
             },
         })
-        return response.data.predictions;
+        return response.data.predictions; // return API response of the list of places
     } catch (error) {
         console.error('Error fetching place suggestions: ', error);
         return []; // emply list
     }
 }
+
+export const getPlaceDetails = async (placeId) => {
+
+};
