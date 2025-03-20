cb4ddbc011e52641b374d32253ba15316dfe221f
asiemsen <siemsenaaron@gmail.com>
2025-03-19 20:19:06 -0500

    WW72 added function to collect preferences data

--- src/backend/dataCollect.js
+++ src/backend/dataCollect.js
@@ -1,1 +1,22 @@
-//init
+
+const preferencesMap = new Map([
+    ["destination", null],
+    ["cuisine", null],
+    ["activityType", null],
+    ["budget", null],
+    ["transportation", null],
+    ["moreDetails", null],
+]);
+
+function collectPreferences (destination = null, cuisine = null, activityType = null, budget = null, transportation = null, moreDetails = null)
+{
+
+    preferencesMap.set("destination", destination);
+    preferencesMap.set("cuisine", cuisine);
+    preferencesMap.set("activityType", activityType);
+    preferencesMap.set("budget", budget);
+    preferencesMap.set("transportation", transportation);
+    preferencesMap.set("moreDetails", moreDetails);
+    
+}
+
