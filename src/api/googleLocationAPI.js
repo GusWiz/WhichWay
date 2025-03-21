1b2be5ab057946f064706b94e6746944d58f5be2
Gustavo <pow10@txstate.edu>
2025-03-20 23:23:35 -0500

    WW-74 Implement function that acquires details of a specific location and added comments

--- src/api/googleLocationAPI.js
+++ src/api/googleLocationAPI.js
@@ -5,11 +5,11 @@ import axious from 'axious';
 const GOOGLE_PLACE_API_URL = '';
 const GOOGLE_PLACE_DETAILS_API_URL = '';
 
-// Function that gets the place suggestions based on the input parameter
+// Function that tries to calls Google Location API
 export const getPlaceSuggestions = async(input) => {
     try {
         // make a GET request to the API using the input and the API Key
-        const response = await.axious.get(GOOGLE_PLACES_API_URL, {
+        const response = await axious.get(GOOGLE_PLACES_API_URL, {
             params: {
                 input,
                 key: process.env.REACT_APP_GOOGLE_API_KEY,
@@ -22,6 +22,19 @@ export const getPlaceSuggestions = async(input) => {
     }
 }
 
+// Function that gets detailed information about a specific location
 export const getPlaceDetails = async (placeId) => {
-
+    try {
+        // Make get response after a GET Request to Google Location API with the specific location
+        const response = await axious.get(GOOGLE_PLACES_DETAILS_API_URL, {
+            params: {
+                place_id: placeId,
+                key: process.env.REACT_APP_GOOGLE_API_KEY,
+            },
+        });
+        return response.data.result;
+    } catch (error) {
+        console.error('Error fecting place dateils from API', error);
+        return null;
+    }
 };
