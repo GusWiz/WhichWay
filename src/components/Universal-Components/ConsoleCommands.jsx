523f8fc9cacb8d60c21d99d76a8ac6f7618d1368
vrsecode <158780886+vrsecode@users.noreply.github.com>
2025-03-21 19:45:20 -0500

    WW-94 Added tutorial inside consolecommands.jsx for anyone who wants to use that file

--- src/components/Universal-Components/ConsoleCommands.jsx
+++ src/components/Universal-Components/ConsoleCommands.jsx
@@ -1,3 +1,29 @@
+// How to use this file:
+// First, in the page that you want to use a console command, create a function inside that page, not this one,
+// that runs the series of commands that you want to test. Then create a new structure called "const cmdPassThru"
+// and it should looks something like this:
+//
+//      const cmdPassthru = {
+//          budgetTest
+//      };
+//
+// where budgetTest is the function I created and want to be used here. This should be inside the App portion
+// of your page but before its return(). 
+//
+// Then, inside the return function, find anywhere to place 
+//
+//      <ConsoleCommands cmdPassThru={cmdPassthru} /> 
+// 
+// like you would any other page component. Doesn't matter where its placed
+//
+// Then, just create a new case inside the switch block where the case is the command you want to type.
+// Run commands by doing 
+//
+//      runCommand("budgetTest")
+//
+// in the console.
+
+
 import React, { useEffect } from "react";
 
     const ConsoleCommands = ({ cmdPassThru }) => {
