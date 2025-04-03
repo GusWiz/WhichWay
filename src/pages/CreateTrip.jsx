46b6f20e38ccb160975f3fa3b6cedc0efb01fa28
Alan De La Torre <sua9@txstate.edu>
2025-04-03 13:44:23 -0500

    hopefull it build bruh

--- src/pages/CreateTrip.jsx
+++ src/pages/CreateTrip.jsx
@@ -6,7 +6,7 @@ import './CreateTrip.css';
 import './CreateItinerary.css';
 import './Home.css';
 import { ToastContainer, toast } from 'react-toastify';
-import { saveUserTrip } from '../api/dataModel.js';
+import { saveUserTrip } from '../components/api/dataModel.js';
 import { auth } from '../components/firebase.js';
 import {
   getSavedActivities,
@@ -24,7 +24,7 @@ import ActivitiesDisplay from '../components/Createtrip-Components/ActivitiesDis
 import ConsoleCommands from '../components/Universal-Components/ConsoleCommands.jsx';
 import LocationSearch from '../components/Createtrip-Components/LocationSearch.jsx';
 import LocationAutocomplete from '../components/Createtrip-Components/LocationAutocomplete';
-import { fetchActivitiesByLocation } from '../api/placesService';
+import { fetchActivitiesByLocation } from '../components/api/placesService.js';
 import FadingTextBox from '../components/Createtrip-Components/FadingTextBox.jsx';
 
 function CreateTrip() {
