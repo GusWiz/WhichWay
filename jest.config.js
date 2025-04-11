ea6fa640e55fbaccdf5414fe71e719016c66bb49
Alan De La Torre <sua9@txstate.edu>
2025-04-11 16:53:44 -0500

    WW-100 got 1 file down

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,7 @@ module.exports = {
   testEnvironment: 'jsdom', // Use jsdom for simulating a browser-like environment
   moduleNameMapper: {
     '\\.css$': '<rootDir>/__mocks__/styleMock.js', // Mock CSS imports
+    '\\.svg$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.css$': 'identity-obj-proxy',
   },
 };
