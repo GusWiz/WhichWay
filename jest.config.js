bcfd78d1fdcf6403b08996f6602d0c298e62efc8
Alan De La Torre <sua9@txstate.edu>
2025-04-11 16:37:44 -0500

    WW-98 added coverage script -alan

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',  // Transpile both .js and .jsx files using babel-jest
+    '^.+\\.[jt]sx?$': 'babel-jest', // Transpile both .js and .jsx files using babel-jest
   },
-  testEnvironment: 'jsdom',  // Use jsdom for simulating a browser-like environment
+  testEnvironment: 'jsdom', // Use jsdom for simulating a browser-like environment
   moduleNameMapper: {
-    '\\.css$': '<rootDir>/__mocks__/styleMock.js',  // Mock CSS imports
+    '\\.css$': '<rootDir>/__mocks__/styleMock.js', // Mock CSS imports
   },
 };
